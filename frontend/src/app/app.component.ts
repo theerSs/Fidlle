eb970c80a6dcce0c7222f183851d9c618197dbb1
jlas <jakublasek41@gmail.com>
2025-03-09 21:06:59 +0100

    Recreat frontend app

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
 }
